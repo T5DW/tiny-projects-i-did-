ba79339515b320a661699361d75c7e1d1eae2136
T5DW <Wyattstark2009@gmail.com>
2026-06-26 06:53:34 -0500

    adds guessing system

--- Python/number guessing game/Main.py
+++ Python/number guessing game/Main.py
@@ -1,5 +1,5 @@
 print("Number Guessing Game By Wyatt Stark!")
-#-----correct stuff!------------------------------------------------------------------
+#-----correct stuff!-and variables-----------------------------------------------------------------
 num1 = 3
 num2 = 7
 num3 = 13
@@ -7,6 +7,9 @@ num4 = 23
 #------------------------point-stuff-----------------------------------------------------------
 reg_points = 0
 points = 1
+#-----------------------guess-counter-------------------------------------------------
+reg_counter = 0
+counter = 1
 #-----------------------difficulty's-------------------------------------------------
 easy = 1
 medium = 2
@@ -14,13 +17,17 @@ hard = 3
 very_hard = 4
 #---------------------Functions----------------------------------------------------
 def start_game1():
+          global  reg_counter
+          reg_counter = 0
           global reg_points
           while True:
                     ask = (int)(input("Guess a number through 1 thru 3: "))
+                    reg_counter += counter
                     if ask == (num1):
                               print("You Beat Game 1!")
                               reg_points += points
                               print("Your points are now: " + str(reg_points))
+                              print("it took you this amount of guesses: " + str(reg_counter))
                               start_game2()
                     elif ask < (num1):
                               print('Too Low!')
@@ -32,13 +39,17 @@ def start_game1():
                               print("You have lost a point: " + str(reg_points))
 #----------------------------------------------------------------------------------
 def start_game2():
+          global  reg_counter
+          reg_counter = 0
           global reg_points
           while True:
                     ask2 =(int)(input("Guess a number through 1 through 8: "))
+                    reg_counter += counter
                     if ask2 == (num2):
                               print("You Beat Game 2!")
                               reg_points += points
                               print("Your points are now: " + str(reg_points))
+                              print("it took you this amount of guesses: " + str(reg_counter))
                               start_game3()
                     elif ask2 < (num2):
                               print("Too Low!")
@@ -50,13 +61,17 @@ def start_game2():
                               print("You have lost a point:" + str(reg_points))
 #----------------------------------------------------------------------------------
 def start_game3():
+          global  reg_counter
+          reg_counter = 0
           global reg_points
           while True:
                     ask3 =(int)(input("Guess a number through 1 through 15: "))
+                    reg_counter += counter
                     if ask3 == (num3):
                               print("You Beat Game 3!")
                               reg_points += points
                               print("Your points are now: " + str(reg_points))
+                              print("it took you this amount of guesses: " + str(reg_counter))
                               start_game4()
                     elif ask3 < (num3):
                               print("Too Low")
@@ -68,13 +83,17 @@ def start_game3():
                               print("You have lost a point: " + str(reg_points))
 #----------------------------------------------------------------------------------
 def start_game4():
+          global  reg_counter
+          reg_counter = 0
           global reg_points
           while True:
                     ask4 =(int)(input("Guess a number through 1 through 30: "))
+                    reg_counter += counter
                     if ask4== (num4):
                               print("You Beat Game 4!")
                               reg_points += points
                               print("Your final points are:  " + str(reg_points))
+                              print("it took you this amount of guesses: " + str(reg_counter))
                               break
                     elif ask4 < (num4):
                               print("Too Low")
