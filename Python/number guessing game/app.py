5dcf3a7f22c9f0ab510ba114f13c4a7d82bd3f2b
T5DW <Wyattstark2009@gmail.com>
2026-06-30 23:14:01 -0500

    Add number guessing game GUI using Tkinter
    game is functional this will be my main priority for the next couple days

---
+++ Python/number guessing game/app.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+def app():
+          
+          root = tk.Tk()
+          root.geometry("500x600")
+          print("App Started!")
+          label = tk.Label(root, text ="Number Guessing Game", font=("Arial", 18 ))
+          label.pack()
+          root.title("Number Guessing Game By Wyatt Stark")
+
+
+
+          label = tk.Label(root, text ="click a number 1 thru 3", font=("Arial", 18 ))
+          label.pack()
+          btnframe = tk.Frame(root)
+          btnframe.columnconfigure(0, weight=1)
+          btnframe.columnconfigure(1, weight=1)
+          btnframe.columnconfigure(2, weight=1)
+
+          popup_label = tk.Label(root, text="", font=("Arial", 16), fg="red")
+          popup_label.pack(pady=20)
+          
+          popup_get = tk.Label(root, text="", font=("Arial", 16), fg="orange")
+          popup_get.pack(pady=20)
+          
+          popup_right = tk.Label(root, text="", font=("Arial", 16), fg="green")
+          popup_right.pack(pady=20)
+
+          def wrong1():
+                    popup_label.config(text="Wrong")
+          def wrong2():
+                    popup_get.config(text="Your getting there")
+          def right1():
+                    popup_right.config(text="Correct")          
+          
+          btn1 = tk.Button(btnframe, text="1", pady= -50 , width= 1, font=("Arial", 18), command=wrong1)
+          btn1.grid(row=0, column=1,   sticky=tk.W+tk.E)
+
+
+          btn2 = tk.Button(btnframe, text="2", pady= -15000 , width= 1, font=("Arial", 18), command=wrong2 )
+          btn2.grid(row=2, column=1, sticky=tk.W+tk.E)
+          
+
+          btn3 = tk.Button(btnframe, text="3", pady= -15000 , width= 1, font=("Arial", 18), command=right1)
+          btn3.grid(row=3, column=1, sticky=tk.W+tk.E)
+          print("Generating Next Level")
+
+          btnframe.pack(fill= 'x')
+          
+          root.mainloop()
+          
+app()
+print('Closed App')
