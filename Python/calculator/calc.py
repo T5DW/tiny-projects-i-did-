b34a3692d34ce8c43c113eddb7b5093d399cd76b
T5DW <Wyattstark2009@gmail.com>
2026-07-13 14:06:27 -0500

    basic addition calculator!

---
+++ Python/calculator/calc.py
@@ -0,0 +1,10 @@
+ask1 =  int(input("Enter Your First Number: "))
+ask2 =   int(input("Enter Your 2nd Number: "))
+
+sum = ask1 + ask2
+
+def addthemup():
+          print("Your Sum is: " + str(sum))
+
+
+addthemup()
