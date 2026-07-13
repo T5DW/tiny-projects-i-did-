fce588f9a6cd5d5c4d9ae164cab07cfd72552048
T5DW <Wyattstark2009@gmail.com>
2026-07-13 14:24:54 -0500

    Refactor calculator to use functions for operations

--- Python/calculator/calc.py
+++ Python/calculator/calc.py
@@ -1,24 +1,31 @@
-#add
-ask_add1 =  int(input("Enter Your First Number: "))
-ask_add2 =   int(input("Enter Your 2nd Number: "))
+#add function
+def add():
+          ask_add1 =  int(input("Enter Your First Number: "))
+          ask_add2 =   int(input("Enter Your 2nd Number: "))
+          sum = ask_add1 + ask_add2
+          print("Your Sum is: " + str(sum))
 #sub function
 def sub():
           ask_sub1 = int(input("Enter Your FIrst Number: "))
           ask_sub2 = int(input("Enter Your Second Number: "))
           total = ask_sub1 - ask_sub2
           print("Your Total is : " + str(total))
-
-#sum for the add
-sum = ask_add1 + ask_add2
-
-#TODO: Make a add function so this code is mainly functions
-def addthemup():
-          print("Your Sum is: " + str(sum))
+def multi():
+          ask_multi1 = int(input("Enter Your First Number: "))
+          ask_multi2 = int(input("Enter Your Second Number: "))
+          total2 = ask_multi1 * ask_multi2
+          print("Your Total is: " + str(total2))
 
 
-addthemup()
+add()
 ask3= input("Do you want to do subtraction? PUT y for yes n for no  ")
 if ask3 == "y": 
           sub()
 elif ask3 == "n":
           print("ok then")
+
+ask4 = input("Do you want to do multiplication? PUT y for yes n for no  ")
+if ask4 == "y":
+          multi()
+elif ask4 == "n":
+          print("bye!")
